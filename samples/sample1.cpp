fa75ebf94492083efc8b8c53781a72572dfa8279
zekailin00 <zekailin00@gmail.com>
2024-04-30 12:11:25 -0700

    transform conversion from blender

--- samples/sample1.cpp
+++ samples/sample1.cpp
@@ -28,43 +28,77 @@ struct CbData
     RD::Buffer rdRTProp;
 };
 
-struct Camera
-{
-    float x, y, z, focal;
-    float wx, wy, wz, exposure;
-};
-
 void render(void* data, unsigned char** image, int* out_width, int* out_height);
 bool RenderSceneConfigUI(CbData *d);
+void rayTracer(std::string& modelFile, std::string& shaderPath,
+    RD::RayTraceProperties& RTProp, RD::PhysicalCamera& camData, RD::SceneProperties& sceneData);
+
+inline float blenderToXRad(float xDeg)
+{
+    xDeg = -90.0 - xDeg;
+    float xRad = xDeg / 180.0 * 3.14159;
+    return xRad;
+}
+
+inline float blenderToYRad(float yDeg)
+{
+    float yRad = yDeg / 180.0 * 3.14159;
+    return yRad;
+}
+
+inline float blenderToZRad(float zDeg)
+{
+    float zRad = zDeg / 180.0 * 3.14159;
+    return zRad;
+}
+
+inline void blenderToCameraRotation(float xDeg, float yDeg, float zDeg, float& xRad, float& yRad, float& zRad)
+{
+    xRad = blenderToXRad(xDeg);
+    yRad = blenderToZRad(zDeg - 180);
+    zRad = blenderToYRad(yDeg - 180);
+}
+
+inline void blenderToCameraTranslate(float x, float y, float z, float& xOut, float& yOut, float& zOut)
+{
+    xOut = xOut;
+    yOut = z;
+    zOut = -y;
+}
+
+RD::DirLight blenderToDirLight(float xDeg, float zDeg, float intensity)
+{
+    float xRad = blenderToXRad(xDeg);
+    float zRad = blenderToZRad(zDeg);
+
+    aiMatrix3x3 eulerX;
+    aiMatrix3x3::Rotation(xRad, {-1,0,0}, eulerX);
+
+    aiMatrix3x3 eulerY;
+    aiMatrix3x3::Rotation(zRad, {0,1,0}, eulerY);
+
+    aiVector3f lightDir = {0, 0, 1};
+    lightDir = eulerX * eulerY * lightDir;
+
+    RD::DirLight light = {
+        .direction = {lightDir[0], lightDir[1], lightDir[2], 0.0f},
+        .color = {intensity, intensity, intensity, 1.0f}
+    };
+    return light;
+}
 
 int main()
 {
     std::string modelFile =
         // "/home/zekailin00/Desktop/ray-tracing/framework/assets/Cornell2.glb"; // helmet
-        "/home/zekailin00/Desktop/ray-tracing/framework/assets/Cornell-armor.glb"; //armor
-        // "/home/zekailin00/Desktop/ray-tracing/framework/assets/Cornell5-mat.glb"; // mat-test
-        // "/home/zekailin00/Desktop/ray-tracing/framework/assets/Cornell6-mat.glb"; // mat-test-tran
-        // "/home/zekailin00/Desktop/ray-tracing/framework/assets/sample1.glb";
+        // "/home/zekailin00/Desktop/ray-tracing/framework/assets/Cornell-armor.glb"; //armor
+        "/home/zekailin00/Desktop/ray-tracing/framework/assets/benchmark/cornell-pbr-standard.glb";
+        // "/home/zekailin00/Desktop/ray-tracing/framework/assets/benchmark/cornell-transmission.glb";
+
     std::string shaderPath = "/home/zekailin00/Desktop/ray-tracing/framework/samples/shader.cl";
 
-    // // sample1
-    // struct Camera camData = {
-    //     .x =  -1.0f,
-    //     .y = 2.0f, 
-    //     .z = 1.0f,
-    //     .focal = -1.0f,
-    //     .wx = -0.0f,
-    //     .wy = -0.6f,
-    //     .wz = -0.0f,
-    //     .exposure = 1.0f
-    // };
-    // RD::SceneProperties sceneData;
-    // sceneData.lightCount[0] = 1;
-    // sceneData.lights[0] = {
-    //     .direction = {100.0f, -10.0f, 10.0f},
-    //     .color = {15.0f, 15.0f, 15.0f, 1.0f}
-    // };
-
+    RD::SceneProperties sceneData;
+    sceneData.lightCount[0] = 1;
 
     // // cornell
     // struct Camera camData = {
@@ -84,59 +118,39 @@ int main()
     //     .color = {10.0f, 10.0f, 10.0f, 1.0f}
     // };
 
-    // // material test
-    // struct Camera camData = {
-    //     .x = 0.0f,
-    //     .y = 12.0f, 
-    //     .z = -12.0f,
-    //     .focal = -1.0f,
-    //     .wx = 0.4f,
-    //     .wy = 3.14f,
-    //     .wz = -0.0f,
-    //     .exposure = 1.0f
-    // };
-    // RD::SceneProperties sceneData;
-    // sceneData.lightCount[0] = 1;
-    // sceneData.lights[0] = {
-    //     .direction = {1.0f, 1.0f, 10.0f},
-    //     .color = {10.0f, 10.0f, 10.0f, 1.0f}
-    // };
+    // material test
+    RD::PhysicalCamera camData = {
+        .widthPixel = 2000.0f,       .heightPixel = 2000.0f,
+        .focalLength = 0.036f,       .sensorWidth = 0.036f,
+        .focalDistance = 2.0f,       .fStop = 0.00f,
+        .x  = 0.00f,  .y  = 6.0f,    .z = -10.0f,
+        .wx = 0.00f,  .wy = 3.1415f, .wz =  0.00f
+    };
+    sceneData.lights[0] = blenderToDirLight(-95.0f, 5.0f, 10.0f);
 
-
-    // cornell - details
-    // struct Camera camData = {
-    //     .x = 0.0f,
-    //     .y = 7.5f, 
-    //     .z = -8.0f,
-    //     .focal = -1.0f,
-    //     .wx = 0.4f,
-    //     .wy = 3.14f,
-    //     .wz = -0.0f,
-    //     .exposure = 1.0f
+    // // cornell - details
+    // RD::PhysicalCamera camData = {
+    //     .widthPixel = 1000.0f,      .heightPixel = 1000.0f,
+    //     .focalLength = 0.030f,      .sensorWidth = 0.036f,
+    //     .focalDistance = 2.0f,      .fStop = 0.00f
     // };
+    // sceneData.lights[0] = blenderToDirLight(-95.0f, 20.0f, 10.0f);
+    // blenderToCameraTranslate(0, 9, 7.5, camData.x, camData.y, camData.z);
+    // blenderToCameraRotation(-115, 180, 0, camData.wx, camData.wy, camData.wz);
 
     /* Define pipeline data inputs */
     RD::RayTraceProperties RTProp = {
-        .totalSamples = 0,
-        .batchSize = 128,
-        .depth = 8,
-        .debug = 0
+        .totalSamples = 0,  .batchSize = 10,
+        .depth = 8,         .debug = 0
     };
 
-    RD::PhysicalCamera camData = {
-        .widthPixel = 1000.0f,      .heightPixel = 1000.0f,
-        .focalLength = 0.0500f,     .sensorWidth = 0.0500f,
-        .focalDistance = 4.0f,      .fStop = 0.10f,
-        .x  = 0.00f,  .y  = 7.20f,  .z = -8.00f,
-        .wx = 0.40f,  .wy = 3.14f,  .wz =  0.00f
-    };
-    RD::SceneProperties sceneData;
-    sceneData.lightCount[0] = 1;
-    sceneData.lights[0] = {
-        .direction = {0.0f, 1.0f, 10.0f},
-        .color = {10.0f, 10.0f, 10.0f, 1.0f}
-    };
 
+    rayTracer(modelFile, shaderPath, RTProp, camData, sceneData);
+}
+
+void rayTracer(std::string& modelFile, std::string& shaderPath,
+    RD::RayTraceProperties& RTProp, RD::PhysicalCamera& camData, RD::SceneProperties& sceneData)
+{
     size_t imageSize = camData.widthPixel * camData.heightPixel * RD_CHANNEL;
     uint8_t* image = (uint8_t*)malloc(imageSize);
 
@@ -211,7 +225,6 @@ int main()
     render((void*)&data, nullptr, nullptr, nullptr);
     stbi_write_jpg(fileName.c_str(), extent[0], extent[1], RD_CHANNEL, data.image, 100);
     printf("Writing image with extent: <%d, %d>\n", extent[0], extent[1]);
-    printf("Writing file to: %s\n", fileName.c_str());
 #else
     renderLoop(render, &data);
 #endif
@@ -228,10 +241,28 @@ void render(void* data, unsigned char** image, int* out_width, int* out_height)
     updated = RenderSceneConfigUI(d);
 #endif
 
+#ifdef OFF_SCREEN
+    time_t start_t, end_t;
+    double diff_t;
+    char* timeStr;
+
+    time(&start_t);
+    timeStr = ctime(&start_t);
+    printf("\nStart of ray tracing: %s", timeStr);
+#endif
+
     RD::TraceRays(d->plt, 0,0,0, d->extent[0], d->extent[1]);
 
     /* Fetch result */
     RD::ReadBuffer(d->plt, d->rdImage, d->imageSize, d->image);
+
+#ifdef OFF_SCREEN
+    time(&end_t);
+    timeStr = ctime(&start_t);
+    printf("End of ray tracing: %s", timeStr);
+    diff_t = difftime(end_t, start_t);
+    printf("Execution time = %f\n", diff_t);
+#endif
 
     /* Update frame sample counts */
     RD::RayTraceProperties RTProp;
@@ -258,7 +289,7 @@ void render(void* data, unsigned char** image, int* out_width, int* out_height)
 bool RenderSceneConfigUI(CbData *d)
 {
     bool updated = false;
-    Camera camData;
+    RD::PhysicalCamera camData;
     RD::ReadBuffer(d->plt, d->rdCamData, sizeof(camData), &camData);
 
     RD::SceneProperties scene;
@@ -273,7 +304,7 @@ bool RenderSceneConfigUI(CbData *d)
         ImGui::Text("Camera:");
         updated |= ImGui::SliderFloat3("Camera Position", &camData.x, -40.0f, 40.0f);
         updated |= ImGui::SliderFloat3("Camera Rotation", &camData.wx, -10.0f, 10.0f);
-        updated |= ImGui::SliderFloat("Camera focal", &camData.focal, -10.0f, 10.0f);
+        updated |= ImGui::SliderFloat("Camera focal", &camData.focalLength, -10.0f, 10.0f);
 
         ImGui::Text("Light:");
         updated |= ImGui::SliderFloat4("Light Direction", scene.lights[0].direction, -100.0f, 100.0f);
